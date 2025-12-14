c2de671302962af5a80872b9490146e020523b84
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-13 21:16:48 -0300

    16.14. Repository orientado a persistência

---
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/InvoiceRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.algashop.billing.domain.model.invoice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
+
+
+
+}
