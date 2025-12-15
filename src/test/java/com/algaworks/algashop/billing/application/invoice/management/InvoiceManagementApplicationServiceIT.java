9bcd86d398c82c67b5008671c55aa3e34c5f0311
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-15 19:43:47 -0300

    16.16. Application Service com CQRS para geração de Invoice

---
+++ src/test/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationServiceIT.java
@@ -0,0 +1,79 @@
+package com.algaworks.algashop.billing.application.invoice.management;
+
+import com.algaworks.algashop.billing.domain.model.creditcard.CreditCard;
+import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardRepository;
+import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardTestDataBuilder;
+import com.algaworks.algashop.billing.domain.model.invoice.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@SpringBootTest
+@Transactional
+class InvoiceManagementApplicationServiceIT {
+
+    @Autowired
+    private InvoiceManagementApplicationService applicationService;
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    @Autowired
+    private CreditCardRepository creditCardRepository;
+
+    @MockitoSpyBean
+    private InvoicingService invoicingService;
+
+    @Test
+    void shouldGenerateInvoice() {
+        CreditCard creditCard = CreditCardTestDataBuilder.aCreditCard().builder();
+        creditCardRepository.saveAndFlush(creditCard);
+
+        GenerateInvoiceInput input = GenerateInvoiceInputTestDataBuilder.anInput().build();
+
+        input.setPaymentSettings(
+                PaymentSettingsInput.builder()
+                        .creditCardId(creditCard.getId())
+                        .method(PaymentMethod.CREDIT_CARD)
+                        .build()
+        );
+
+        UUID invoiceId = applicationService.generate(input);
+
+        Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow();
+
+        Assertions.assertThat(invoice.getStatus()).isEqualTo(InvoiceStatus.UNPAID);
+        Assertions.assertThat(invoice.getOrderId()).isEqualTo(input.getOrderId());
+
+        Mockito.verify(invoicingService).issue(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+
+    }
+
+    @Test
+    void shouldGenerateInvoiceWithGatewayBalanceAsPayment() {
+        GenerateInvoiceInput input = GenerateInvoiceInputTestDataBuilder.anInput().build();
+
+        input.setPaymentSettings(
+                PaymentSettingsInput.builder()
+                        .method(PaymentMethod.GATEWAY_BALANCE)
+                        .build()
+        );
+
+        UUID invoiceId = applicationService.generate(input);
+
+        Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow();
+
+        Assertions.assertThat(invoice.getStatus()).isEqualTo(InvoiceStatus.UNPAID);
+        Assertions.assertThat(invoice.getOrderId()).isEqualTo(input.getOrderId());
+
+        Mockito.verify(invoicingService).issue(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+
+    }
+
+}
