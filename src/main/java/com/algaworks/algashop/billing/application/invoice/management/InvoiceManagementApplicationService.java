46041f4e9f442bb7fa53c04880c8409eb253f8ab
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-17 15:37:40 -0300

    16.17. Application Service para pagamento de Invoice

--- src/main/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationService.java
@@ -3,7 +3,9 @@ package com.algaworks.algashop.billing.application.invoice.management;
 import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardNotFoundException;
 import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardRepository;
 import com.algaworks.algashop.billing.domain.model.invoice.*;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.Payment;
 import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentGatewayService;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -37,6 +39,37 @@ public class InvoiceManagementApplicationService {
         invoiceRepository.saveAndFlush(invoice);
 
         return invoice.getId();
+    }
+
+    @Transactional
+    public void processPayment(UUID invoiceId) {
+        Invoice invoice = invoiceRepository.findById(invoiceId).orElseThrow(InvoiceNotFoundException::new);
+        PaymentRequest paymentRequest = toPaymentRequest(invoice);
+
+        Payment payment;
+        try {
+            payment = paymentGatewayService.capture(paymentRequest);
+        } catch (Exception e) {
+            String errorMessage = "Payment capture failed";
+            log.error(errorMessage, e);
+            invoice.cancel(errorMessage);
+            invoiceRepository.saveAndFlush(invoice);
+            return;
+        }
+
+        invoicingService.assignPayment(invoice, payment);
+        invoiceRepository.saveAndFlush(invoice);
+
+    }
+
+    private PaymentRequest toPaymentRequest(Invoice invoice) {
+        return PaymentRequest.builder()
+                .amount(invoice.getTotalAmount())
+                .method(invoice.getPaymentSettings().getMethod())
+                .creditCardId(invoice.getPaymentSettings().getCreditCardId())
+                .payer(invoice.getPayer())
+                .invoiceId(invoice.getId())
+                .build();
     }
 
     private Set<LineItem> convertToLineItems(Set<LineItemInput> itemsInput) {
--- src/test/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationServiceIT.java
@@ -4,11 +4,16 @@ import com.algaworks.algashop.billing.domain.model.creditcard.CreditCard;
 import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardRepository;
 import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardTestDataBuilder;
 import com.algaworks.algashop.billing.domain.model.invoice.*;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.Payment;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentGatewayService;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentRequest;
+import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentStatus;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,6 +34,9 @@ class InvoiceManagementApplicationServiceIT {
 
     @MockitoSpyBean
     private InvoicingService invoicingService;
+
+    @MockitoBean
+    private PaymentGatewayService paymentGatewayService;
 
     @Test
     void shouldGenerateInvoice() {
@@ -74,6 +82,30 @@ class InvoiceManagementApplicationServiceIT {
 
         Mockito.verify(invoicingService).issue(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
 
+    }
+
+    @Test
+    void shouldProcessInvoicePayment() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.changePaymentSettings(PaymentMethod.GATEWAY_BALANCE, null);
+        invoiceRepository.saveAndFlush(invoice);
+
+        Payment payment = Payment.builder()
+                .gatewayCode("12345")
+                .invoiceId(invoice.getId())
+                .method(invoice.getPaymentSettings().getMethod())
+                .status(PaymentStatus.PAID)
+                .build();
+        Mockito.when(paymentGatewayService.capture(Mockito.any(PaymentRequest.class))).thenReturn(payment);
+
+        applicationService.processPayment(invoice.getId());
+
+        Invoice paidInvoice = invoiceRepository.findById(invoice.getId()).orElseThrow();
+
+        Assertions.assertThat(paidInvoice.isPaid()).isTrue();
+
+        Mockito.verify(paymentGatewayService).capture(Mockito.any(PaymentRequest.class));
+        Mockito.verify(invoicingService).assignPayment(Mockito.any(Invoice.class), Mockito.any(Payment.class));
     }
 
 }
