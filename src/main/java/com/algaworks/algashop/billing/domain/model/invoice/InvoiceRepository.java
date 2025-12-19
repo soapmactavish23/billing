842d23d45b05a149e8ccafcf58a2b280eb4e40e9
henrick.nogueira <ricknogueira1231@gmail.com>
2025-12-18 22:15:22 -0300

    16.18. Consultas com QueryService

--- src/main/java/com/algaworks/algashop/billing/domain/model/invoice/InvoiceRepository.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/invoice/InvoiceRepository.java
@@ -2,10 +2,12 @@ package com.algaworks.algashop.billing.domain.model.invoice;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, UUID> {
 
     Boolean existsByOrderId(String orderId);
+    Optional<Invoice> findByOrderId(String orderId);
 
 }
---
+++ src/test/java/com/algaworks/algashop/billing/application/invoice/query/InvoiceQueryServiceIT.java
@@ -0,0 +1,35 @@
+package com.algaworks.algashop.billing.application.invoice.query;
+
+import com.algaworks.algashop.billing.domain.model.invoice.Invoice;
+import com.algaworks.algashop.billing.domain.model.invoice.InvoiceRepository;
+import com.algaworks.algashop.billing.domain.model.invoice.InvoiceTestDataBuilder;
+import com.algaworks.algashop.billing.domain.model.invoice.PaymentMethod;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class InvoiceQueryServiceIT {
+
+    @Autowired
+    private InvoiceQueryService invoiceQueryService;
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    @Test
+    void shouldFindByOrderId() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.changePaymentSettings(PaymentMethod.GATEWAY_BALANCE, null);
+        invoiceRepository.saveAndFlush(invoice);
+        InvoiceOutput invoiceOutput = invoiceQueryService.findByOrderId(invoice.getOrderId());
+
+        Assertions.assertThat(invoiceOutput.getId()).isEqualTo(invoice.getId());
+    }
+
+}
