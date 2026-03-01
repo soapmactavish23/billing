11c3460b86ac2648f6abd5a388263d8d7cf1aae0
soapmactavish23 <ricknogueira1231@gmail.com>
2026-03-01 00:27:52 -0300

    7.10. Camada de Application para Credit Card

--- src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
+++ src/main/java/com/algaworks/algashop/billing/domain/model/creditcard/CreditCardRepository.java
@@ -2,7 +2,11 @@ package com.algaworks.algashop.billing.domain.model.creditcard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CreditCardRepository extends JpaRepository<CreditCard, UUID> {
+    Optional<CreditCard> findByCustomerIdAndId(UUID customerId, UUID creditCardId);
+    List<CreditCard> findAllByCustomerId(UUID customerId);
 }
